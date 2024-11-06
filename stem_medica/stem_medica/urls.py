67c9d8d1115632b6e1bc8c11c63a8b18033b0af9
Logos <132839177+logoskal@users.noreply.github.com>
2024-11-06 22:56:40 +0300

    Collected Static Files and a bunch of other stuff

--- stem_medica/stem_medica/urls.py
+++ stem_medica/stem_medica/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('config/', admin.site.urls, name='admin-page'),
     path('', include('home.urls')),
     path('', include('users.urls'))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
