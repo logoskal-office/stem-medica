842d3f8d2e384dda7c79286a5b8d2d8c34324024
Logos <132839177+logoskal@users.noreply.github.com>
2024-11-08 00:26:32 +0300

    The Search Bar is working

--- stem_medica/products/urls.py
+++ stem_medica/products/urls.py
@@ -3,6 +3,6 @@ from .views import publish_product, products_view
 
 urlpatterns = [
     path('publish/', publish_product, name='publish-product-page'),
-    path('products/',products_view ),
+    path('products/',products_view, name='products-page'),
     
 ]
