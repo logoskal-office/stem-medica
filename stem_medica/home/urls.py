c4fff9a276ffd34cba35c50891fa85ce6a7de0fc
Logos <132839177+logoskal@users.noreply.github.com>
2024-11-07 15:49:22 +0300

    Created A Download View

--- stem_medica/home/urls.py
+++ stem_medica/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
-from .views import contact_us, about_us
+from .views import contact_us, about_us, download
 
 urlpatterns = [
     path('contact-us/', contact_us, name='contact-us-page'),
     path('about-us/', about_us, name='about-us-page'),
+    path('download/<str:file_name>', download, name='download')
 ]
--- stem_medica/home/views.py
+++ stem_medica/home/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from django.http import FileResponse, Http404
 
-def contact_us(request):
+import os
+from django.conf import settings
+
+def contact_us(request, ):
     return render(request, 'home/contact-us.html')
 
 def about_us(request):
     return render(request, 'home/about-us.html')
+
+def download(request, file_name):
+    if not file_name:
+        return render(request, 'download.html')
+
+    file_path = os.path.join(settings.MEDIA_ROOT, 'files/'+ file_name)
+    
+    if not os.path.exists(file_path):
+        raise Http404('Requested File Not Found')
+    
+    response = FileResponse(open(file_path, 'rb'))
+    response['Content-Type'] = 'application/octet-stream'  # Optional: to force download
+    return response
+
